5529b7f82f2e21b907058b521f2be811095189e4
Andrey Bronin <andrey.bronin@insolar.io>
2019-06-03 17:47:11 +0300

    try to fix travis

--- build.py
+++ build.py
@@ -3,6 +3,6 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager("andreybronin")
-    #builder.add_common_builds()
-    builder.add(settings={"arch": "x86_64", "build_type": "Debug"}, options={"Shared", False}, env_vars={}, build_requires={})
+    builder.add_common_builds()
+    #builder.add(settings={"arch": "x86_64", "build_type": "Debug"}, options={"Shared", False}, env_vars={}, build_requires={})
     builder.run()
